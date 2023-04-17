5970561a4729d7e664c6541513b51dad444c0f47
olvuk <radenovic-family@yandex.ru>
2023-04-17 16:08:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+
+    }
+
+    public static void task1() {
+
+    }
+    public static void task2() {
+
+    }
+    public static void task3() {
+
+    }
+    public static void task4() {
+
+    }
+    public static void task5() {
+
+    }
+    public static void task6() {
+
+    }
+    public static void task7() {
+
+    }
+    public static void task8() {
+
+    }
+    public static void task9() {
+
+    }
+    public static void task10() {
+
+    }
+}
